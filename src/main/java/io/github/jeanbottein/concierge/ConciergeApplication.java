0cfaf6e1820c0e78bec3fc6f678b9d7c4e0f39bd
Steam Deck User <deck@steamdeck.lan>
2025-03-09 20:36:55 -0400

    Initial commit: Spring Boot project setup

---
+++ src/main/java/io/github/jeanbottein/concierge/ConciergeApplication.java
@@ -0,0 +1,13 @@
+package io.github.jeanbottein.concierge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConciergeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConciergeApplication.class, args);
+	}
+
+}
